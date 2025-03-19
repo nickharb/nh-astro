0f1e94792e21c06ced1c3fd9ef5cd955e919c481
Nick Harbaugh <nick@periscopic.com>
2025-03-19 14:51:20 -0500

    Update output to static in config

--- .vercel/output/functions/_render.func/dist/server/chunks/adaptation-atlas_DtDNhUW8.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./adaptation-atlas_BQxXii9p.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
--- .vercel/output/functions/_render.func/dist/server/chunks/systems-change-lab_D249Fbqf.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./systems-change-lab_mKH87p_u.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
